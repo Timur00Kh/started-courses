0e4aa2bc27aa5bc16debcd74b68bf2f37e2f0541
Timur00Kh <newt300@mail.ru>
2020-07-28 18:52:56 +0300

    Updates

---
+++ precache-manifest.bfb61ce15322d6ae30a67afcd48a459b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125dde1a14811c42a23cf19ba608e0dc",
+    "url": "/started-courses/index.html"
+  },
+  {
+    "revision": "beb01e0552d9ea4dfa7e",
+    "url": "/started-courses/static/css/main.16e65349.chunk.css"
+  },
+  {
+    "revision": "856f07ff5f9d806d71cb",
+    "url": "/started-courses/static/js/2.8bbe3745.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/started-courses/static/js/2.8bbe3745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb01e0552d9ea4dfa7e",
+    "url": "/started-courses/static/js/main.36b4d61e.chunk.js"
+  },
+  {
+    "revision": "2eacc985624d01414eb2",
+    "url": "/started-courses/static/js/runtime-main.9e77fca4.js"
+  },
+  {
+    "revision": "3345ad51891c7dbc5868f1f10ae40e6b",
+    "url": "/started-courses/static/media/Museosanscyrl500.3345ad51.woff2"
+  },
+  {
+    "revision": "d0c7ec833dd85c52fb59a59babe6e1ba",
+    "url": "/started-courses/static/media/Museosanscyrl500.d0c7ec83.ttf"
+  },
+  {
+    "revision": "d97960c3800c88cb8ccab3527443ef97",
+    "url": "/started-courses/static/media/Museosanscyrl500.d97960c3.woff"
+  },
+  {
+    "revision": "c60a188935180179587c4c6e693529f2",
+    "url": "/started-courses/static/media/add.c60a1889.svg"
+  },
+  {
+    "revision": "6385915d7c94579cd2af8831823e730d",
+    "url": "/started-courses/static/media/burger.6385915d.svg"
+  },
+  {
+    "revision": "a0509c9177cb5ef79d975b69d53d1803",
+    "url": "/started-courses/static/media/collapse.a0509c91.svg"
+  },
+  {
+    "revision": "314264c46d56d2e52481ccaf033dc951",
+    "url": "/started-courses/static/media/delete.314264c4.svg"
+  },
+  {
+    "revision": "3ab8affd9aea8671d3e02f5de217f9a1",
+    "url": "/started-courses/static/media/img.3ab8affd.svg"
+  },
+  {
+    "revision": "745f84634048c592a1fdbe96cf348219",
+    "url": "/started-courses/static/media/load.745f8463.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/started-courses/static/media/logo.5d5d9eef.svg"
+  }
+]);
